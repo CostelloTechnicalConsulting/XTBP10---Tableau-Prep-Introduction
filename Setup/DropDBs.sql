c5687d1e1ec1f1132dc20bb010d4431dadfe746e
Dan Costello <dancos@gmail.com>
2020-01-25 01:19:05 -0500

    updated db scripts

---
+++ Setup/DropDBs.sql
@@ -0,0 +1,2 @@
+DROP DATABASE [Sandbox]
+DROP DATABASE [AdventureWorksLT] 
--- Setup/RestoreAdventureWorksLT.sql
+++ Setup/RestoreAdventureWorksLT.sql
@@ -1,11 +1,8 @@
-DECLARE @DataDirPath nvarchar = N'C:\XTBP10ClassFiles\Data\DBFiles';
-DECLARE @DataFilePath nvarchar(max) = @DataDirPath + '\AdventureWorksLT2012.mdf';
-DECLARE @DataLogPath nvarchar(max) = @DataDirPath + '\AdventureWorksLT2012_log.ldf';
-
 USE [master]
 RESTORE DATABASE [AdventureWorksLT] 
 FROM  DISK = N'C:\XTBP10ClassFiles\Data\AdventureWorksLT2016.bak' 
-WITH  FILE = 1,  MOVE N'AdventureWorksLT2012_Data' TO @DataFilePath,  MOVE N'AdventureWorksLT2012_Log' TO @DataLogPath,  NOUNLOAD,  REPLACE,  STATS = 5
+WITH  FILE = 1,  MOVE N'AdventureWorksLT2012_Data' TO 'C:\XTBP10ClassFiles\Data\DBFiles\AdventureWorksLT2012.mdf',  
+MOVE N'AdventureWorksLT2012_Log' TO 'C:\XTBP10ClassFiles\Data\DBFiles\AdventureWorksLT2012_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5
 
 GO
 
